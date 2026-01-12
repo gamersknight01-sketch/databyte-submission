878b71866e01d8148f5c73f544f8f23ef0bbe7ae
gamersknight01-sketch <gamersknight01@gmail.com>
2026-01-12 21:19:41 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,15 +1,13 @@
 import pandas as pd
 from ydata_profiling import ProfileReport
 
-# Load the training data
 df = pd.read_csv(r"C:\Users\megha\PycharmProjects\databyte\train.csv")
 
-# Generate profiling report
 profile = ProfileReport(
     df,
     title="Train Data Profiling Report",
     explorative=True
 )
 
-# Save report to HTML
+
 profile.to_file("train_data_profiling.html")
